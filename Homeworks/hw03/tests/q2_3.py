ccd426aa68b530e38ad91c6eac54c6d82f064bab
zijingdi <zidi@ucsd.edu>
2019-01-23 12:48:00 -0800

    release hw03

---
+++ Homeworks/hw03/tests/q2_3.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 2_3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(shortest, np.ndarray)
+          True
+          >>> len(shortest) == 10
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Homeworks/hw03/tests/q3_2.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 3_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(web_percent, float)
+          True
+          >>> 0 <= web_percent <= 1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
