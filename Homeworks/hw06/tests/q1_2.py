a5762750d60a73fcfc3e9d84fa696efd88e5d3b3
zijingdi <zidi@ucsd.edu>
2019-02-19 16:56:12 -0800

    release hw6

---
+++ Homeworks/hw06/tests/q1_2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(greater_than_650, float)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Homeworks/hw06/tests/q5_4.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 5_4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(p_value_5_4, float)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
