facbd6b11c3ab869e1c25083c02a792ccc2b73d8
zijingdi <zidi@ucsd.edu>
2019-02-23 11:05:38 -0800

    release project02

---
+++ Projects/diet_and_disease/tests/q2_3_1.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 2.3.1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> import numbers
+          >>> isinstance(totchol_mean_with_chd, numbers.Real)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
